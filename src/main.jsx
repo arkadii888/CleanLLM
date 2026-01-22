d535dd519c7787a0db6e9bbc4f71450605a592c7
arkadii888 <arkadii.personal@proton.me>
2026-01-22 01:53:43 +0100

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// test
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
